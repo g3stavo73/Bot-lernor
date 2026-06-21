e3b8b28cedae13de0fd1be2c71fefa9685b3862c
Gusta <g3stavo73@gmail.com>
2026-06-21 01:52:39 -0300

    Initial commit from Acode

---
+++ Decision.js
@@ -0,0 +1,13 @@
+const name = "pedro"
+function name_verify() {
+  if (name === "pedro") {
+    console.log("PEDRINHOOOOOO!!")
+  } 
+  else if (name = "João") {
+    console.log("amigo de pedro, mas vai embora!")
+  }
+  else {
+    console.log("Sai")
+  }
+  
+}
---
+++ main.rs
@@ -0,0 +1,3 @@
+let age = u32
+let mut name = "CARLINHOS CARAMELO DO RUST"
+let mut code = u21;
